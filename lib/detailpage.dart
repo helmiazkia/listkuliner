e0f1e9ef215da46dc15a6eb7b4b07d1c1497fe07
Helmi azkia <azkiasekian@gmail.com>
2024-10-23 18:03:26 +0700

    membenarkan bug

--- lib/detailpage.dart
+++ lib/detailpage.dart
@@ -1,103 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:listkuliner/makanan.dart'; // Import model makanan
 
 class DetailPage extends StatelessWidget {
-  final Makanan makanan; // Menerima data makanan
-
-  const DetailPage({super.key, required this.makanan});
+  const DetailPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Stack(
-              children: <Widget>[
-                Image.asset(
-                  makanan.gambar, // Gambar dari objek makanan
-                  width: double.infinity, // Lebar penuh layar
-                  fit: BoxFit.cover, // Mengatur gambar agar sesuai dengan layar
-                ),
-                SafeArea(
-                  child: Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        CircleAvatar(
-                          backgroundColor: Colors.grey,
-                          child: IconButton(
-                            icon: const Icon(
-                              Icons.arrow_back,
-                              color: Colors.white,
-                            ),
-                            onPressed: () {
-                              Navigator.pop(context); // Aksi kembali ke halaman sebelumnya
-                            },
-                          ),
-                        ),
-                        const FavouriteButton(), // Tombol favorit
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            
-            // Penempatan Container untuk menampilkan nama makanan
-            Container(
-              margin: const EdgeInsets.only(top: 16.0), // Margin di atas
-              child: Text(
-                makanan.nama, // Nama makanan
-                textAlign: TextAlign.center, // Teks ditengah
-                style: const TextStyle(
-                  fontSize: 24.0, // Ukuran teks
-                  fontWeight: FontWeight.bold, // Teks tebal
-                ),
-              ),
-            ),
-            
-            // Konten lain di bawah nama makanan seperti deskripsi, detail, dll
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                makanan.deskripsi, // Deskripsi makanan
-                style: const TextStyle(fontSize: 16.0),
-                textAlign: TextAlign.justify,
-              ),
-            ),
-            // Tambahkan konten lain seperti informasi kalori, harga, dll.
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class FavouriteButton extends StatefulWidget {
-  const FavouriteButton({super.key});
-
-  @override
-  _FavouriteButtonState createState() => _FavouriteButtonState();
-}
-
-class _FavouriteButtonState extends State<FavouriteButton> {
-  bool isFavourite = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return IconButton(
-      icon: Icon(
-        isFavourite ? Icons.favorite : Icons.favorite_border,
-        color: isFavourite ? Colors.red : Colors.white,
-      ),
-      onPressed: () {
-        setState(() {
-          isFavourite = !isFavourite;
-        });
-      },
-    );
+    return const Placeholder();
   }
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,61 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:listkuliner/List_item.dart';
-import 'package:listkuliner/styles.dart';
-import 'makanan.dart'; // Import model makanan
+import 'package:listkuliner/list_item.dart';
+import 'package:listkuliner/makanan.dart'; // Import Makanan class
 
 class HomePage extends StatelessWidget {
-  final List<Makanan> listMakanan; // Definisikan variabel listMakanan
-
-  // Konstruktor untuk menerima listMakanan
-  const HomePage({super.key, required this.listMakanan});
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('List Kuliner', style: textHeader1),
-        centerTitle: true,
-      ),
-      body: ListView.builder(
-        // Mengatur panjang / jumlah item dalam list
-        itemCount: listMakanan.length, // Jumlah item sesuai panjang listMakanan
-        padding: const EdgeInsets.all(10),
-        itemBuilder: (context, index) {
-          // Membangun item list menggunakan ListItem
-          return Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: Card(
-              elevation: 5,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: ListTile(
-                leading: Image.asset(
-                  listMakanan[index].gambar, // Menampilkan gambar makanan
-                  width: 70,
-                  height: 70,
-                  fit: BoxFit.cover,
-                ),
-                title: Text(
-                  listMakanan[index].nama,
-                  style: textHeader2,
-                ), // Nama makanan
-                subtitle: Text(listMakanan[index].deskripsi), // Deskripsi makanan
-                onTap: () {
-                  // Aksi saat item ditekan, misal pindah ke halaman detail
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) =>
-                          ListItem(makanan: listMakanan[index]),
-                    ),
-                  );
-                },
-              ),
-            ),
-          );
-        },
-      ),
+    return ListView.builder(
+      // Access the static list from Makanan class
+      itemCount: Makanan.listMakanan.length,
+      padding: const EdgeInsets.all(10),
+      itemBuilder: (context, index) {
+        return ListItem(makanan: Makanan.listMakanan[index]);
+      },
     );
   }
 }
