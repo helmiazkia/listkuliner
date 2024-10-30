a59cd8ca65b5a1e528b1a8a1f6a6382466b8f350
Helmi azkia <azkiasekian@gmail.com>
2024-10-30 16:14:41 +0700

    fovourite button

--- lib/detailpage.dart
+++ lib/detailpage.dart
@@ -3,8 +3,46 @@ import 'package:flutter/material.dart';
 class DetailPage extends StatelessWidget {
   const DetailPage({super.key});
 
+  
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: const Color(0xFFFFF3E0),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Stack(),
+            Container(
+                //...
+                ),
+            Container(
+                //...
+                ),
+            Container(
+                //...
+                ),
+            SizedBox(
+                //...
+                ),
+            SizedBox(
+                //...
+                ),
+            Container(
+                //...
+                ),
+            SizedBox(
+                //...
+                ),
+            Container(
+                //...
+                ),
+            SizedBox(
+                //...
+                )
+          ],
+        ),
+      ),
+    );
   }
 }
